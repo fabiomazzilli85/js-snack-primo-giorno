e60ea114e61c41b91e410ab47c949a14cf3398ef
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-01 13:16:46 +0100

    Tre esercizi JS + Bonus.

---
+++ javascript/app.js
@@ -0,0 +1,45 @@
+ // Esercizio 1
+
+const number = 100;  //number 
+console.log (number)
+
+ // Fine esercizio 1
+
+// Esercizio 2
+
+const PI = 3.14; //number
+console.log (PI)
+
+// Fine esecizio 2
+
+// Esercizio 2B
+
+// const PI = 3.1416; 
+// console.log (PI)
+
+ // Come immaginavo la console del browser dà un messaggio di errore perché "PI" è già stato dichiarato precedenetemente.
+ // Commento il codice dell'esercizio 2B perché, se non lo facessi, dà messaggioi di errore anche al codice sottostante. 
+
+// // Esercizio 2C
+
+let radius = 8;
+let circle= radius * 2 * Math.PI;
+
+console.log (circle)
+
+// Esercizio 3
+
+let nome = "Carlo";  //String
+nome = "Marco";  //String
+
+console.log(nome);
+
+// Esercizio a piacere
+
+const mioNome = "Fabio";  //String
+const mioCognome = "Mazzilli";  //String
+let età = 39; //Number
+età++;
+
+console.log (mioNome + ' ' + mioCognome) // Concatenazione di stringhe
+console.log (età)
